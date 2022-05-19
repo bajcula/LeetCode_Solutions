f2273f2bf6aa1ac3dd7946c4b1c4badc7464f4d4
Branislav Todorovic <97913469+bajcula@users.noreply.github.com>
2022-05-19 18:10:51 -0500

    Time: 55 ms (88.21%), Space: 13.9 MB (32.01%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        
+        richest = 0
+        
+        for row in accounts:
+            total = 0
+            for amount in row:
+                total += amount
+            if total > richest:
+                richest = total
+                
+        return richest
