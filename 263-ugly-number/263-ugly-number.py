b426eaf06e76e9838a80258a4ad69fd328df8a28
Branislav Todorovic <97913469+bajcula@users.noreply.github.com>
2022-05-25 11:53:56 -0500

    Time: 49 ms (39.88%), Space: 13.9 MB (57.41%) - LeetHub

--- 263-ugly-number/263-ugly-number.py
+++ 263-ugly-number/263-ugly-number.py
@@ -3,13 +3,13 @@ class Solution:
         
         if n == 0: return False
         
-        while n % 2 == 0:
-            n = n // 2
+        while n % 5 == 0:
+            n = n // 5
 
         while n % 3 == 0:
             n = n // 3
-
-        while n % 5 == 0:
-            n = n // 5
+    
+        while n % 2 == 0:
+            n = n // 2
         
         return n == 1
