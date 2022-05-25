bc7d170edbd142c8897f492a6103f0a6123716d1
Branislav Todorovic <97913469+bajcula@users.noreply.github.com>
2022-05-25 12:18:40 -0500

    Time: 39 ms (68.67%), Space: 13.9 MB (59.10%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -3,37 +3,12 @@ class Solution:
         
         n = len(digits)
         
-        for i in range(n - 1, -1, -1):            
-
+        for i in range(n - 1, -1, -1):
             if digits[i] != 9:
-                
                 digits[i] += 1
-                
-                return digits
-                
+                return digits 
             else:
                 digits[i] = 0
                 
         return [1] + digits
-    
-    
-    
-#         digits = digits[::-1]
-#         remainder = 1
-#         i = 0
-        
-#         while remainder:
-#             if i < len(digits):
-#                 if digits[i] == 9:
-#                     digits[i] = 0
-#                 else:
-#                     digits[i] += 1
-#                     remainder = 0
-#             else:
-#                 digits.append(1)
-#                 remainder = 0
-#             i += 1
-            
-#         return digits[::-1]
-        
         
