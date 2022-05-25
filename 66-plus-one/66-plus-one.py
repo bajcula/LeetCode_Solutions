7348ca2e9269951d8ba0dd345810bac2a183822f
Branislav Todorovic <97913469+bajcula@users.noreply.github.com>
2022-05-25 12:14:48 -0500

    Time: 47 ms (42.88%), Space: 13.8 MB (59.10%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,41 +1,39 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
-#         n = len(digits)
+        n = len(digits)
         
-#         for i in range(n - 1, -1, -1):            
-            
-#             remainder = 0 if digits[i] != 9 else 1
-            
-            
-            
-#             if digits[i] != 9:
-#                 digits[i] = digits[i] + remainder
-#                 remainder = 0
+        for i in range(n - 1, -1, -1):            
+
+            if digits[i] != 9:
                 
-#             else:
-#                 digits[i] = 0
-#                 remainder = 1
+                digits[i] += 1
                 
-#         return digits
-    
-    
-    
-        digits = digits[::-1]
-        remainder = 1
-        i = 0
-        
-        while remainder:
-            if i < len(digits):
-                if digits[i] == 9:
-                    digits[i] = 0
-                else:
-                    digits[i] += 1
-                    remainder = 0
+                return digits
+                
             else:
-                digits.append(1)
-                remainder = 0
-            i += 1
+                digits[i] = 0
+                
+        return [1] + digits
+    
+    
+    
+#         digits = digits[::-1]
+#         remainder = 1
+#         i = 0
         
-        return digits[::-1]
+#         while remainder:
+#             if i < len(digits):
+#                 if digits[i] == 9:
+#                     digits[i] = 0
+#                 else:
+#                     digits[i] += 1
+#                     remainder = 0
+#             else:
+#                 digits.append(1)
+#                 remainder = 0
+#             i += 1
+            
+#         return digits[::-1]
+        
         
