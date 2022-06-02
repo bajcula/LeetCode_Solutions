2d7bfecdb89801c6acf0b0d961a3a7eb790c649e
Branislav Todorovic <97913469+bajcula@users.noreply.github.com>
2022-06-02 14:46:04 -0500

    Time: 39 ms (87.67%), Space: 14.1 MB (48.87%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -4,15 +4,11 @@ class Solution:
         nums.sort()
         res = []
         
-        def backtrack(i = 0, solution = []):
-            
+        def backtrack(i = 0, solution = []):    
             res.append(solution[:])
-            
             for j in range(i, len(nums)):
-
                 if j > i and nums[j] == nums[j - 1]:
-                    continue
-                    
+                    continue    
                 solution.append(nums[j])
                 backtrack(j + 1, solution)
                 solution.pop()
