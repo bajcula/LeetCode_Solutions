c7b166d70b3c4c4670dc9a624e30801bc2bbf06c
Branislav Todorovic <97913469+bajcula@users.noreply.github.com>
2022-06-02 14:26:48 -0500

    Time: 240 ms (36.37%), Space: 14.4 MB (75.44%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.py
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -1,19 +1,32 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         
-        count = 0
+#         count = 0
         
-        for i in range(len(flowerbed)):
+#         for i in range(len(flowerbed)):
 
-            if flowerbed[i] == 0:
+#             if flowerbed[i] == 0:
  
-                empty_left_plot = (i == 0) or (flowerbed[i - 1] == 0)
-                empty_right_lot = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+#                 empty_left_plot = (i == 0) or (flowerbed[i - 1] == 0)
+#                 empty_right_lot = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
                 
-                if empty_left_plot and empty_right_lot:
-                    flowerbed[i] = 1
-                    count += 1
-                    if count == n:
-                        return True
+#                 if empty_left_plot and empty_right_lot:
+#                     flowerbed[i] = 1
+#                     count += 1
+#                     if count == n:
+#                         return True
                     
-        return count >= n
+#         return count >= n
+    
+    
+        l = len(flowerbed)
+        bed = [0] + flowerbed + [0]
+        
+        for i in range(1, l + 1):
+            if bed[i] == bed[i - 1] == bed[i + 1] == 0:
+                bed[i] = 1
+                n -= 1
+            
+            if n <= 0: return True
+        
+        return False
